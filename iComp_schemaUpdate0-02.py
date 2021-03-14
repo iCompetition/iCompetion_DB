a62187c8a0367402fe295c5676498247a21ad16d
sean <smapps001@gmail.com>
2021-03-14 06:36:48 -0500

    fixing schema and ordering issue

--- iComp_schemaUpdate0-02.py
+++ iComp_schemaUpdate0-02.py
@@ -22,8 +22,22 @@ ALTER TABLE `scoring`
   ADD COLUMN `posGain` int(11) NOT NULL AFTER `qualPosition`;
 
 ALTER TABLE `event`
-  ADD COLUMN `enableHardChargerBonus` int(11) NOT NULL AFTER `live`;
+  ADD COLUMN `enableHardChargerBonus` int(11) NOT NULL AFTER `winner`;
+
+DROP TABLE IF EXISTS `iComp_schema`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `iComp_schema` (
+  `version` text CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `iComp_schema` WRITE;
+/*!40000 ALTER TABLE `iComp_schema` DISABLE KEYS */;
+INSERT INTO `iComp_schema` VALUES ('0.02');
+/*!40000 ALTER TABLE `iComp_schema` ENABLE KEYS */;
+UNLOCK TABLES;
 """
+
 
 fh = open("./iComp_schemaUpdate0-02.sql",'w')
 fh.write(useDB)
